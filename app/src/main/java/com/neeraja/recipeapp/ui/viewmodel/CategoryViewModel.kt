d2e2592fecc1235bff16dece4be8fca96e743a2d
apple <gaandlaneeraja@gmail.com>
2020-12-16 20:35:21 +0530

    Setup ViewModel for Categories

--- app/src/main/java/com/neeraja/recipeapp/ui/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/neeraja/recipeapp/ui/viewmodel/CategoryViewModel.kt
@@ -1,5 +1,39 @@
 package com.neeraja.recipeapp.ui.viewmodel
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mindorks.framework.mvvm.utils.Resource
+import com.neeraja.recipeapp.data.model.Category
+import com.neeraja.recipeapp.data.repository.CategoryRepository
+import com.neeraja.recipeapp.utils.NetworkHelper
+import kotlinx.coroutines.launch
 
-class CategoryViewModel : ViewModel()
+class CategoryViewModel @ViewModelInject constructor(
+    private val categoryRepository: CategoryRepository,
+    private val networkHelper: NetworkHelper
+) : ViewModel() {
+    private val _categories = MutableLiveData<Resource<List<Category>>>()
+
+    val categories: LiveData<Resource<List<Category>>>
+        get() = _categories
+
+    init {
+        fetchCategories()
+    }
+
+    private fun fetchCategories() {
+        viewModelScope.launch {
+            _categories.postValue(Resource.loading(null))
+            if (networkHelper.isNetworkConnected()) {
+                categoryRepository.getCategories().let {
+                    if (it.isSuccessful) {
+                        _categories.postValue(Resource.success(it.body()))
+                    } else _categories.postValue(Resource.error(it.errorBody().toString(), null))
+                }
+            } else _categories.postValue(Resource.error("No Internet Connection", null))
+        }
+    }
+}
