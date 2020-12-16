1285f072fc701ec00bd567190261cc2e563e8f9f
apple <gaandlaneeraja@gmail.com>
2020-12-16 21:02:10 +0530

    Setup Activity for Categories

--- app/src/main/java/com/neeraja/recipeapp/ui/view/CategoryActivity.kt
+++ app/src/main/java/com/neeraja/recipeapp/ui/view/CategoryActivity.kt
@@ -2,11 +2,68 @@ package com.neeraja.recipeapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mindorks.framework.mvvm.utils.Status
 import com.neeraja.recipeapp.R
+import com.neeraja.recipeapp.data.model.Category
+import com.neeraja.recipeapp.ui.adapter.CategoryAdapter
+import com.neeraja.recipeapp.ui.viewmodel.CategoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_category.*
 
+@AndroidEntryPoint
 class CategoryActivity : AppCompatActivity() {
+    private val categoryViewModel : CategoryViewModel by viewModels()
+    private lateinit var adapter: CategoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+        setupUI()
+        setupObserver()
     }
+
+    private fun setupUI() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = CategoryAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupObserver() {
+        categoryViewModel.categories.observe(this, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    progressBar.visibility = View.GONE
+                    it.data?.let { users -> renderList(users) }
+                    recyclerView.visibility = View.VISIBLE
+                }
+                Status.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                    recyclerView.visibility = View.GONE
+                }
+                Status.ERROR -> {
+                    //Handle Error
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    private fun renderList(users: List<Category>) {
+        adapter.addData(users)
+        adapter.notifyDataSetChanged()
+    }
+
 }
