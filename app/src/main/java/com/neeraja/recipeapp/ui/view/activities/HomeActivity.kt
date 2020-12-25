8108067718fc9cc0f9a08be76abeadf7c2fe8ca3
apple <gaandlaneeraja@gmail.com>
2020-12-25 20:38:39 +0530

    Setup Databinding in Activities

--- app/src/main/java/com/neeraja/recipeapp/ui/view/activities/HomeActivity.kt
+++ app/src/main/java/com/neeraja/recipeapp/ui/view/activities/HomeActivity.kt
@@ -2,15 +2,23 @@ package com.neeraja.recipeapp.ui.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
 import com.neeraja.recipeapp.R
+import com.neeraja.recipeapp.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var bookmarksIv: ImageView
+    private lateinit var wishlistIv: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
+        bookmarksIv = binding.ivBookmarks
+        wishlistIv = binding.ivWishlist
     }
 
 }
--- app/src/main/java/com/neeraja/recipeapp/ui/view/activities/SplashActivity.kt
+++ app/src/main/java/com/neeraja/recipeapp/ui/view/activities/SplashActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.databinding.DataBindingUtil
 import com.neeraja.recipeapp.R
+import com.neeraja.recipeapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        val binding =
+            DataBindingUtil.setContentView<ActivitySplashBinding>(this, R.layout.activity_splash)
 
         Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
